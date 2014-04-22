1b1c108ad089b5b350990e66e106f1e3bca9b036
Hildebrando Rueda <harq.soft@gmail.com>
2014-04-22 00:10:17 -0500

    Story 3: fix the test failing due time mismatch

--- test/unit/deal_test.rb
+++ test/unit/deal_test.rb
@@ -7,9 +7,11 @@ class DealTest < ActiveSupport::TestCase
 
   # I think this is a bad test and it fails sometimes
   test "over should honor current time" do
-  	deal = FactoryGirl.create(:deal, :end_at => Time.zone.now + 0.01)
+  	deal = FactoryGirl.create(:deal, :end_at => Time.zone.now + 1.minute)
   	assert !deal.over?, "Deal should not be over"
-  	sleep 1
-  	assert deal.over?, "Deal should be over"
+
+    Timecop.travel(Time.zone.now + 1.minute) do
+      assert deal.over?, "Deal should be over"
+    end
   end
 end
